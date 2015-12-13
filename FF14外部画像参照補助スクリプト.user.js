6a5c8894c103638a380d911b231754ea189ee5f0
NohohonNohon <mahorabas@yahoo.co.jp>
2015-12-13 17:48:23 +0900

    アップロードせずに画像URLだけ取得出来る機能追加
    アップロードせずに画像URLだけ取得出来る機能追加

--- FF14外部画像参照補助スクリプト.user.js
+++ FF14外部画像参照補助スクリプト.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        FF14外部画像参照補助スクリプト
 // @namespace   nohohon
-// @description FF14の外部画像参照機能をアップロードから登録まで行えるようにする
+// @description FF14の外部画像参照機能を楽に登録できるようにするスクリプト
 // @include     http://jp.finalfantasyxiv.com/lodestone/my/blog/post/*
 // @include     https://picasaweb.google.com/*
 // @include     https://docs.google.com/picker?protocol=iframes*
-// @version     1
+// @version     1.1.0
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
@@ -13,7 +13,7 @@
 // @grant       GM_registerMenuCommand
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getResourceText
-// @resource   usconfigcss https://raw.github.com/NohohonNohon/ff14ExternalScript/master/usconfig.css.template
+// @resource    usconfigcss https://raw.github.com/NohohonNohon/ff14ExternalScript/master/usconfig.css.template
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     https://raw.github.com/NohohonNohon/ff14ExternalScript/master/usconfig.js
 // ==/UserScript==
@@ -29,10 +29,12 @@
         
         /** ウィンドウ表示方法 */
         var OPEN_WINDOW_TYPE = {popup:'ポップアップ',tab:'新規タブ'};
-        
+        /** 画像のアップロード有無 */
+        var UPLOAD_FLAG = {true:'画像をアップロードする',false:'画像をアップロードしない'};
+
     //共通変数
         /** 設定値 */
-        var settings = {upload_URL:'',user_ID:'',album_ID:'',open_type:''};
+        var settings = {upload_URL:'',user_ID:'',album_ID:'',open_type:'',popup_width:'',popup_height:'',upload_flag:''};
     
     
     //==================================================
@@ -56,27 +58,36 @@
                 Config.open();
                 return false;
             }
-            var target_window;
+            try {
+                if(FF14.target_window.closed == false){
+                    return false;
+                }
+            } catch(e) {
+                //target_windowが無くエラーになることがあるが何もしない
+            }
             if (settings.open_type ==  OPEN_WINDOW_TYPE.popup){
-                target_window = window.open(settings.upload_URL,UPLOAD_WINDOW_NAME,'toolbar=no,titlebar=no,status=no,scrollbars=yes,resizable=yes,menubar=no,location=no');
+                var param = 'width=' + settings.popup_width + ',height=' + settings.popup_height + 
+                    ',toolbar=no,titlebar=no,status=no,scrollbars=yes,resizable=yes,menubar=no,location=no';
+                FF14.target_window = window.open(settings.upload_URL,UPLOAD_WINDOW_NAME,param);
             } else if(settings.open_type ==  OPEN_WINDOW_TYPE.tab) {
-                target_window = window.open(settings.upload_URL,UPLOAD_WINDOW_NAME,'');
+                FF14.target_window= window.open(settings.upload_URL,UPLOAD_WINDOW_NAME,'');
             }
             //開いた画面が閉じるのを監視する。
             setTimeout(function loop(){
                 try {
-                    if(target_window.closed == false){
-                        setTimeout(loop,100);
+                    if(FF14.target_window.closed == false){
+                        setTimeout(loop,1000);
                     } else {
                         FF14.setImgDataURL();
                     }
                 } catch(err) {
                     FF14.setImgDataURL();
                 }
-            },100);
+            },1000);
         };
         
         var gloabl = {
+            target_window:'',
             
             /**
              * FF14ページ関連処理の初期処理
@@ -99,7 +110,6 @@
                     $('#external_file_uri').val(img_list_array[i]);
                     $('#external_file_select').click();
                 }
-                $('#uploadfiles')[0].click();
                 return true;
             }
         };
@@ -111,7 +121,8 @@
     /** Picasaのページ関連処理 */
     //==================================================
     var Picasa = (function() {
-        var COMPLEATED_MSG = 'get Image URL Compleated';
+        var EXIT_MSG = 'EXIT';
+        var CANCEL_MSG = 'CANCEL';
         
         /** 
         * OKボタンクリック時、アップロード枚数をカウントし、getNewImg()を呼び出す
@@ -122,8 +133,20 @@
             if (disabled_state == 'true') {
                 return false;
             }
-            Picasa.img_upload_count = $("div[role ='listbox']").children().children().length;            
+            Picasa.img_upload_count = $("div[role ='listbox']").children().children().length;
+            if (Picasa.img_upload_count > 10) {
+                alert('写真の枚数が10枚を超えています。\n10枚以下にしてください。');
+                return false;
+            }
             getNewImg();
+        };
+        
+        /** 
+        * キャンセルボタンクリック時、画面を閉じるようにする
+        * @param {event} イベント
+        */
+        function clickCancelBtn(e) {
+            window.parent.postMessage(CANCEL_MSG, 'https://picasaweb.google.com');
         };
         
         /** 
@@ -153,7 +176,7 @@
                         }
                         GM_setValue(IMG_LIST_ARRAY,img_array);
                         //親ページに処理完了を伝える
-                        window.parent.postMessage(COMPLEATED_MSG, 'https://picasaweb.google.com');
+                        window.parent.postMessage(EXIT_MSG, 'https://picasaweb.google.com');
                     } catch(err) {
                         alert('画像のURL取得に失敗しました。');
                         console.log(err);
@@ -178,8 +201,9 @@
         
         /** 
         * アルバムの公開状態を確認する
+        * @param {function} コールバック関数(省略可) 
         */
-        function checkAlbumPublic() {
+        function checkAlbumPublic(call_back_func) {
             var url = 'https://picasaweb.google.com/data/feed/api/user/' + 
                             settings.user_ID+ '/albumid/' + settings.album_ID +
                             '?alt=json&max-results=1';
@@ -189,6 +213,8 @@
             onload: function(res) {
                     if (res.responseText == 'No album found.') {
                         alert('アルバムが公開されていません。\nアルバムを一般公開状態にしてください。');
+                    } else {
+                        call_back_func();
                     }
                 },
                 onerror: function(res) {
@@ -208,14 +234,48 @@
         };
         
         /** 
+        * 画像をアップロードしない設定の場合、枚数を指定して新着画像URLを取得する
+        */
+        function getNewImgCount() {
+            //アップロードフラグの確認
+            console.log('TEST');
+            if(settings.upload_flag == UPLOAD_FLAG.false) {
+                console.log('TEST2');
+                var count;
+                while (count == null){
+                    console.log('TEST3');
+                    count = window.prompt('新着画像URL取得枚数を入力してください。(1から10までの数値)', '');
+                    console.log(count);
+                    if(count == null) {
+                        alert('新着画像URL取得をキャンセルします。');
+                        window.close();
+                        break;
+                    }
+                    if(count.match(/[^0-9]+/)){
+                        alert('1から10までの数値を入力してください。');
+                        count = null;
+                    } else if(count < 1 || count > 10) {
+                        alert('1から10までの数値を入力してください。');
+                        count = null;
+                    } else {
+                        Picasa.img_upload_count = count;
+                        getNewImg();
+                    }
+                }
+            }
+        }
+        
+        /** 
         * 子ページの要求により、画面を閉じる
         * @param {event} イベント
         */
         function closeWindow(e) {
-            if(e.originalEvent.data != COMPLEATED_MSG) {
-                return false;
+            if(e.originalEvent.data == EXIT_MSG) {
+                window.close();
+            } else if(e.originalEvent.data == CANCEL_MSG) {
+                //ページ遷移時画面を閉じる
+                $(window).on('beforeunload', function() {window.close();});
             }
-            window.close();
         }
         
         var global = {
@@ -226,9 +286,9 @@
             * Picasaページ関連処理の初期処理
             */
             initPicasa: function() {
-                //ページ遷移時画面を閉じる
-                $(window).on('beforeunload', function() {window.close();});
-                //画像URL取得成功後画面を閉じる
+                //キャンセルボタンクリック時のメッセージを変更
+                $('#lhid_pickerredirect').text('写真のアップロードをキャンセルしています...');
+                //子ページの要求に応じて画面を閉じる
                 $(window).on('message', closeWindow);
                 //1000枚を超えそうな場合警告
                 var img_count = $('#lhid_albumcontains').text();
@@ -236,7 +296,7 @@
                     alert('アルバム内の画像枚数が1000枚を超えそうです。\n1000枚を超えると画像のURLが取得できません。');
                 }
                 //アルバムの公開状態を確認
-                checkAlbumPublic();
+                checkAlbumPublic(getNewImgCount);
             },
             
             /** 
@@ -257,6 +317,14 @@
                         $(this).off('mousemove');
                     }
                 })
+                //キャンセルボタンにクリックイベントを設定
+                //キャンセルボタンが生成されるタイミングが不明のため、マウスムーブのたびにチェックする
+                $(window).on('mousemove',function() {
+                    var cancel_btn = $('#picker\\:ap\\:1');
+                    if(cancel_btn[0] != null) {
+                        cancel_btn.on('click',clickCancelBtn);
+                    }
+                })
             }
         };
         return global;
@@ -266,27 +334,42 @@
     //==================================================
     /** 設定関連処理 */
     //==================================================
+    //http://d.hatena.ne.jp/h1mesuke/20100713/p1
     (function() {
         Config.define('setting', function() {
             with (this.builder) {
-            var open_option = [
+            var open_type = [
                 'ポップアップ',
                 '新規タブ'
-            ]
+            ];
+            var upload_flag = [
+                '画像をアップロードする',
+                '画像をアップロードしない'
+            ];  
             dialog(
-                'コンフィグ',
-                { width: 500, height: 300 },
-                section(
-                        'アップロード',
-                        'アップロードに関する設定をします。',
-                        grid(
-                            text('アップロード画面URL:', 'upload_URL', '', { size: 40 }), '\n',
-                            text('ユーザID:', 'user_ID', '', { size: 40 }), '\n',
-                            text('アルバムID:', 'album_ID', '', { size: 40 }), '\n',
-                            select('表示方法', 'open_type', open_option, 'ポップアップ')
-                        )
+                    'コンフィグ',
+                    { width: 500, height: 500 },
+                    section(
+                            'アップロード',
+                            'アップロードに関する設定をします。',
+                            grid(
+                                text('アップロード画面URL:', 'upload_URL', '', { size: 40 }), '\n',
+                                text('ユーザID:', 'user_ID', '', { size: 40 }), '\n',
+                                text('アルバムID:', 'album_ID', '', { size: 40 }), '\n',
+                                select('画像アップロード有無', 'upload_flag', upload_flag, '画像をアップロードする')
+                            )
+                    ),
+                    section(
+                            '画面表示',
+                            '画面表示に関する設定をします。',
+                            grid(
+                                select('表示方法', 'open_type', open_type, 'ポップアップ'), '\n',
+                                staticText("ポップアップ時のウィンドウサイズ"), '\n',
+                                number( "幅", 'popup_width', 800), '\n',
+                                number( "高さ", 'popup_height', 600)
+                            )
                     )
-                );
+            );
             }
         }, {
             saveKey: 'GM_config',
@@ -345,14 +428,13 @@
     function main() {
         //ページ毎の初期処理呼び出す
         var this_domain = window.location.host;
-        var window_name = window.name;
         if(this_domain == 'jp.finalfantasyxiv.com') {
             FF14.init();
             return true;
         }
         switch (this_domain){
             case 'picasaweb.google.com':
-                if(window_name != UPLOAD_WINDOW_NAME) {
+                if(window.location.href != settings.upload_URL) {
                     return false;
                 }
                 Picasa.initPicasa();
